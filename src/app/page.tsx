5889143ca6faf01cf25984e9c59f16598fd162d5
munaray <munahdesigns@gmail.com>
2024-09-18 14:24:06 +0100

    updating layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import Header from "@/components/Header";
 import SideNav from "@/components/SideNav";
 
 export default function Home() {
+	const handleSelectedIndex = (value: number) => {
+		console.log("Selected Index", value);
+	};
 	return (
 		<>
 			<Header />
-			<div className="w-64 fixed">
-				<SideNav selectedIndex={(value) => console.log(value)} />
-			</div>
-			<div className="ml-64">Body</div>
+			<figure className="w-64 fixed">
+				<SideNav selectedIndex={handleSelectedIndex} />
+			</figure>
+			<figure className="ml-64 grid grid-cols-1 md:grid-cols-6">
+				<div className="md:col-span-2 bg-green-200">Control Panel</div>
+				<div className="md:col-span-3 bg-red-100">Icon Preview</div>
+				<div className="md:col-span-1 bg-blue-100">Ads Banner</div>
+			</figure>
 		</>
 	);
 }
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,7 +3,11 @@
 import { Image, PencilRuler, Shield } from "lucide-react";
 import React, { useState } from "react";
 
-const SideNav = (selectedIndex: string) => {
+interface SideNavProps {
+	selectedIndex: (index: number) => void;
+}
+
+const SideNav: React.FC<SideNavProps> = ({ selectedIndex }) => {
 	const MENU_LIST = [
 		{
 			id: 1,
@@ -22,19 +26,23 @@ const SideNav = (selectedIndex: string) => {
 		},
 	] as const;
 
-	const [activeIndex, setActiveIndex] = useState(0);
+	const [activeIndex, setActiveIndex] = useState<number>(0);
+
+	const handleClick = (index: number) => {
+		setActiveIndex(index);
+		selectedIndex(index);
+	};
 	return (
 		<section className="border shadow-sm h-screen">
 			<div>
 				{MENU_LIST.map((menu, index) => (
 					<h2
 						key={index}
-						onClick={() => {
-							setActiveIndex(index);
-							selectedIndex(index);
-						}}
+						onClick={() => handleClick(index)}
 						className={`p-3 text-lg px-7 text-gray-500 my-2 cursor-pointer hover:bg-blue-700 hover:text-white flex items-center gap-2 ${
-							activeIndex === index && "bg-blue-700 text-white"
+							activeIndex === index
+								? "bg-blue-700 text-white"
+								: ""
 						}`}>
 						<menu.icon className="h-5 w-5" />
 						{menu.name}
